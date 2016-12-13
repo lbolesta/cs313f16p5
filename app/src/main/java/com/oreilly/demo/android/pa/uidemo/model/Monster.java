8ecbd3199a6dc77f1ad9ee860419a2cfb3141133
shadeerafati <srafati@luc.edu>
2016-12-13 16:22:15 -0600

    android studio problems, still errors

---
+++ app/src/main/java/com/oreilly/demo/android/pa/uidemo/model/Monster.java
@@ -0,0 +1,31 @@
+package com.oreilly.demo.android.pa.uidemo.model;
+
+public class Monster {
+    public int i, j; // position in grid
+    public int x, y;
+    public int r; // radius
+
+    public enum State {
+        vulnerble, prot
+    }
+    public State state;
+
+    public Monster (int _i, int _j, int _x, int _y, int _r) {
+        i = _i;
+        j = _j;
+        x = _x;
+        y = _y;
+        r = _r;
+
+        state = State.vulnerble;
+    }
+
+    public void setXY(int i, int j) {
+        x = 150 + 250*j;
+        y = 150 + 250*i;
+    }
+
+    public boolean canBeEliminated() {
+        return state == State.vulnerble;
+    }
+}
